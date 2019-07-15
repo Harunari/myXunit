ad5b7e8ee62319b82a65d5e5e3da3330e259b648
haruanri <nanteneus@hotmail.co.jp>
2019-07-15 13:16:54 +0900

    Implement Test suite function

--- myXunit/Program.cs
+++ myXunit/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace myXunit
@@ -7,66 +8,101 @@ namespace myXunit
     {
         static void Main(string[] args)
         {
-            new TestCaseTest("TestTemplateMethod").Run();
-            new TestCaseTest("TestResult").Run();
-            new TestCaseTest("TestFailedResultFormatting").Run();
-            new TestCaseTest("TestFailedResult").Run();
-            new TestCaseTest("TestFailedSetUpFormatting").Run();
-            new TestCaseTest("TestFailedSetUp").Run();
-            new TestCaseTest("TestTearDownWhenTestFailed").Run();
+            var suite = new TestSuite();
+            suite.Add(new TestCaseTest("TestTemplateMethod"));
+            suite.Add(new TestCaseTest("TestResult"));
+            suite.Add(new TestCaseTest("TestFailedResult"));
+            suite.Add(new TestCaseTest("TestFailedResultFormatting"));
+            suite.Add(new TestCaseTest("TestSuite"));
+            var result = new TestResult();
+            suite.Run(result);
+            Console.WriteLine(result.Summary());
         }
     }
 
     class TestCaseTest : TestCase
     {
         private WasRun test;
+        private readonly TestResult result = new TestResult();
 
         public void TestTemplateMethod()
         {
             test = new WasRun("TestMethod");
-            test.Run();
-            Console.WriteLine($"TestTemplateMethod: {"SetUp TestMethod TearDown" == test.Log}");
+            test.Run(result);
+            if ("SetUp TestMethod TearDown" == test.Log) { return; }
+            throw new AssertException();
         }
         public void TestResult()
         {
             test = new WasRun("TestMethod");
-            var result = test.Run();
-            Console.WriteLine($"TestResult: {"1 run, 0 failed" == result.Summary()}");
+            test.Run(result);
+            if ("1 run, 0 failed" == result.Summary()) { return; }
+            throw new AssertException();
         }
         public void TestFailedResult()
         {
             test = new WasRun("TestBrokenMethod");
-            var result = test.Run();
-            Console.WriteLine($"TestFailedResult: {"1 run, 1 failed" == result.Summary()}");
+            test.Run(result);
+            if ("1 run, 1 failed" == result.Summary()) { return; }
+            throw new AssertException();
         }
         public void TestFailedResultFormatting()
         {
-            var result = new TestResult();
             result.TestStarted();
             result.TestFailed();
-            Console.WriteLine($"TestFailedResultFormatting: {"1 run, 1 failed" == result.Summary()}");
+            if ("1 run, 1 failed" == result.Summary()) { return; }
+            throw new AssertException();
         }
         public void TestFailedSetUpFormatting()
         {
-            var result = new TestResult();
             var error = new Exception();
             result.SetUpFailed(error);
-            Console.WriteLine($"TestFailedSetUpFormatting: {$"SetUp is Failed: {error.Message}" == result.Summary()}");
+            if ($"SetUp is Failed: {error.Message}" == result.Summary()) { return; }
+            throw new AssertException();
         }
         public void TestFailedSetUp()
         {
             test = new BrokenSetUp("");
-            var result = test.Run();
-            Console.WriteLine($"TestFailedSetUp: {result.Summary().StartsWith("SetUp is Failed:")}");
+            test.Run(result);
+            if (result.Summary().StartsWith("SetUp is Failed:")) { return; }
+            throw new AssertException();
         }
         public void TestTearDownWhenTestFailed()
         {
             test = new WasRun("TestBrokenMethod");
-            test.Run();
-            Console.WriteLine($"TestTearDownWhenTestFailed: {test.Log.Contains("TearDown")}");
+            test.Run(result);
+            if (test.Log.Contains("TearDown")) { return; }
+            throw new AssertException();
+        }
+        public void TestSuite()
+        {
+            var suite = new TestSuite();
+            suite.Add(new WasRun("TestMethod"));
+            suite.Add(new WasRun("TestBrokenMethod"));
+            suite.Run(result);
+            if ("2 run, 1 failed" == result.Summary()) { return; }
+            throw new AssertException();
         }
 
-        public TestCaseTest(string methodName) : base(methodName) {}
+        public TestCaseTest(string methodName) : base(methodName) { }
+    }
+
+    internal class TestSuite
+    {
+        List<TestCase> Tests { get; } = new List<TestCase>();
+
+        internal void Add(TestCase test)
+        {
+            Tests.Add(test);
+        }
+
+        internal void Run(TestResult result)
+        {
+            foreach (var test in Tests)
+            {
+                test.Run(result);
+            }
+        }
     }
 
     public class TestResult
@@ -82,7 +118,7 @@ namespace myXunit
         public string Summary()
         {
             if (!string.IsNullOrEmpty(ErrorMessage)) { return ErrorMessage; }
-            
+
             return $"{RunCount} run, {ErrorCount} failed";
         }
 
@@ -96,7 +132,7 @@ namespace myXunit
             ErrorMessage = $"SetUp is Failed: {ex.Message}";
         }
     }
-    public class BrokenSetUp: WasRun
+    public class BrokenSetUp : WasRun
     {
         public BrokenSetUp(string methodName) : base(methodName) { }
         public override void SetUp()
@@ -135,9 +171,8 @@ namespace myXunit
 
         public virtual void SetUp() { }
         public virtual void TearDown() { }
-        public　TestResult Run()
+        public void Run(TestResult result)
         {
-            var result = new TestResult();
             result.TestStarted();
             try
             {
@@ -146,7 +181,6 @@ namespace myXunit
             catch (Exception ex)
             {
                 result.SetUpFailed(ex);
-                return result;
             }
             try
             {
@@ -157,7 +191,17 @@ namespace myXunit
                 result.TestFailed();
             }
             TearDown();
-            return result;
         }
+    }
+
+
+    public class AssertException : Exception
+    {
+        public AssertException() { }
+        public AssertException(string message) : base(message) { }
+        public AssertException(string message, Exception inner) : base(message, inner) { }
+        protected AssertException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
